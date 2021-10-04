d64b5031da239b51fca0ec2e1209d98dd093af46
root <root@odoo14-dev.internal.verdigado.net>
2021-10-04 22:39:11 +0200

    initial commit

---
+++ models/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+# from odoo import models, fields, api
+
+
+# class verdigado_template_default(models.Model):
+#     _name = 'verdigado_template_default.verdigado_template_default'
+#     _description = 'verdigado_template_default.verdigado_template_default'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record in self:
+#             record.value2 = float(record.value) / 100
