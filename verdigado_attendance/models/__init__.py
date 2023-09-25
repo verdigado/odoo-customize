13d4fb4afa51a1fcf64ae8f518730c7b3cb8fdaf
Holger Brunn <mail@hunki-enterprises.com>
2023-09-25 17:48:40 +0200

    [IMP] cut off seconds for attendances and breaks

--- verdigado_attendance/models/__init__.py
+++ verdigado_attendance/models/__init__.py
@@ -1,5 +1,6 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from . import hr_attendance
+from . import hr_attendance_break
 from . import hr_attendance_report
 from . import hr_leave_type
--- verdigado_attendance/tests/__init__.py
+++ verdigado_attendance/tests/__init__.py
@@ -3,3 +3,4 @@
 
 from . import test_hr_access
 from . import test_overtime_calculation
+from odoo.addons.hr_attendance_break.tests import test_hr_attendance_break
