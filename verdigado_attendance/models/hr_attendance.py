b425a140b9db7ace4af3db19163658859e5baf89
Alexander Bigga <alexander@bigga.de>
2023-11-30 14:30:50 +0100

    Revert "[ADD] keep 0 overtime records"

--- verdigado_attendance/models/hr_attendance.py
+++ verdigado_attendance/models/hr_attendance.py
@@ -10,28 +10,3 @@ class HrAttendance(models.Model):
 
     check_in = DatetimeWithoutSeconds()
     check_out = DatetimeWithoutSeconds()
-
-    def _update_overtime(self, employee_attendance_dates=None):
-        """Recreate missing overtime records"""
-        result = super()._update_overtime(
-            employee_attendance_dates=employee_attendance_dates
-        )
-        if employee_attendance_dates is None:
-            employee_attendance_dates = self._get_attendances_dates()
-        missing_vals = []
-        for employee, attendance_dates in employee_attendance_dates.items():
-            dates = [attendance_date for _dummy, attendance_date in attendance_dates]
-            existing_overtime = self.env["hr.attendance.overtime"].search(
-                [
-                    ("employee_id", "=", employee.id),
-                    ("company_id", "=", self.env.company.id),
-                    ("date", "in", dates),
-                ]
-            )
-            missing_vals += [
-                {"employee_id": employee.id, "date": attendance_date}
-                for attendance_date in set(dates)
-                - set(existing_overtime.mapped("date"))
-            ]
-        self.env["hr.attendance.overtime"].create(missing_vals)
-        return result
