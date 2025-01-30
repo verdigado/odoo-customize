9f01c4a6a05b99ae824ecb94670802e6330b5a4d
Alexander Bigga <alexander.bigga@verdigado.com>
2025-01-30 10:36:31 +0100

    Remove blank lines

--- hr_holidays_overlap/models/hr_leave.py
+++ hr_holidays_overlap/models/hr_leave.py
@@ -1,11 +1,9 @@
 # Copyright 2023 Hunki Enterprises BV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0)
 
-
 from odoo import api, models
 
 exclude_sentinel = object()
-
 
 class HrLeave(models.Model):
     _inherit = "hr.leave"
