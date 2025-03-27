f9e8d0f1c8c08af95ff8238bc634bba15b76fbf5
Alexander Bigga <alexander.bigga@verdigado.com>
2025-03-27 15:41:52 +0100

    [16.0] verdigado_template_default: fix labels of custom fields

--- verdigado_template_default/models/res_partner.py
+++ verdigado_template_default/models/res_partner.py
@@ -4,6 +4,6 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = "res.partner"
 
-    x_gliederungsid = fields.Integer()
-    x_kunde_hosting = fields.Boolean()
+    x_gliederungsid = fields.Integer(string="Gliederungs-ID")
+    x_kunde_hosting = fields.Boolean(string="Hostingkunde")
     x_hauptdomain = fields.Char(string="Hauptdomain", store=True, index=True)
