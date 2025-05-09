6f1bfb5a41275f5e51098ae74a331ebea90f90b8
Alexander Bigga <alexander.bigga@verdigado.com>
2025-05-09 16:33:19 +0200

    Task/mig 18 tempaltes (#124)
    * [18.0] enable mail debranding template again
    
    * [18.0] template sfor sale_order_line_sequence not necessary anymore
    
    * [18.0] add custom invoice report modification (WIP)

--- verdigado_template_default/__manifest__.py
+++ verdigado_template_default/__manifest__.py
@@ -25,10 +25,9 @@ verdigado-Layout für odoo
     ],
     "data": [
         "views/debrand_web.xml",
-        # "views/debrand_mail.xml",
+        "views/debrand_mail.xml",
         "views/res_partner.xml",
-        # "views/order_line_number.xml",
-        # "views/report_invoice.xml",
+        "views/report_invoice.xml",
         # "views/sepa_invoice.xml",
     ],
     "assets": {
